d8e009d530b884085f45af8c1566a89e9bf1ae14
mfunyu <mfunyu@student.42lyon.fr>
2024-06-17 17:15:45 +0200

    add: basic Item component

--- front/src/components/dashboard/Dashboard.vue
+++ front/src/components/dashboard/Dashboard.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="flex flex-col gap-5">
     <span class="text-indigo-600 font-bold text-2xl">Dashboard</span>
-    <ul>
-      <li
+    <div class="flex flex-wrap">
+      <Item
         v-for="entity in entities"
-        :key="entity.id">
-        {{ entity.name }}
-      </li>
-    </ul>
+        :key="entity.id"
+        :item="entity" />
+    </div>
   </div>
 </template>
 
 <script>
 import coreApi from "@/providers/core-api"
+import Item from "@/components/items/Item"
 
 export default {
   name: "Dashboard",
+  components: { Item },
   created() {
     this.getEntities()
   },
@@ -23,10 +24,10 @@ export default {
     return {
       entities: [],
       isLoading: false,
-      isError: false 
+      isError: false
     }
   },
-  methods: { 
+  methods: {
     getEntities() {
       this.isLoading = true
 
@@ -42,6 +43,6 @@ export default {
           this.isLoading = false
         })
     }
-  } 
+  }
 }
 </script>
---
+++ front/src/components/items/Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="item-container flex flex-col m-3 py-2 px-5 rounded-md bg-gray-200 shadow-sm">
+    <!-- <img
+      :src="item.image"
+      :alt="item.name"
+      class="w-32 h-32 object-cover rounded-full" /> -->
+    <h2 class="mt-4 text-gray font-bold">{{ item.name }}</h2>
+    <p class="mt-2 text-gray-600">{{ item.status }}</p>
+    <p class="mt-2 text-gray-600">{{ item.description }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Item",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.item-container {
+  width: 25%;  /* Set your desired width */
+  min-width: 200px;
+}
+</style>
