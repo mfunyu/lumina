f80e6e85f8eb88ed0c47d2e8db8b6635d9ff0027
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-18 00:24:06 +0200

    update: add icons, values for items

--- front/src/components/items/Item.vue
+++ front/src/components/items/Item.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="item-container flex flex-col m-3 py-2 px-5 rounded-md bg-gray-200 shadow-sm">
-    <!-- <img
-      :src="item.image"
-      :alt="item.name"
-      class="w-32 h-32 object-cover rounded-full" /> -->
-    <h2 class="mt-4 text-gray font-bold">{{ item.name }}</h2>
-    <p class="mt-2 text-gray-600">{{ item.status }}</p>
-    <p class="mt-2 text-gray-600">{{ item.description }}</p>
+  <div class="item-container flex m-3 py-2 px-5 rounded-md bg-gray-200 shadow-sm">
+    <div>
+      <h2 class="mt-4 text-gray font-bold">{{ item.name }}</h2>
+      <p class="mt-2 text-gray-600">{{ item.status }}</p>
+      <p class="mt-2 text-gray-600">{{ item.description }}</p>
+    </div>
+    <lightbulboff
+      v-if="item.type === 'light' && item.status === 'off'"
+      class="my-auto"
+      :size="60"
+      :fillColor="'gray'" />
+    <lightbulb
+      v-else-if="item.type === 'light' && item.status === 'on'"
+      class="my-auto"
+      :size="60"
+      :fillColor="'yellow'" />
+    <lightswitch
+      v-else-if="item.type === 'switch' && item.status === 'on'"
+      class="my-auto"
+      :size="60"
+      :fillColor="'yellow'" />
+    <lightswitchoff
+      v-else-if="item.type === 'switch' && item.status === 'off'"
+      class="my-auto"
+      :size="60"
+      :fillColor="'gray'" />
+    <div
+      v-else-if="item.value"
+      class="my-auto flex"
+      :style="{ 'min-width': '60px' }">
+      <p class="m-auto text-3xl text-gray-600">{{ item.value }}</p>
+      <span
+        v-if="item.type === 'air_conditioner'"
+        class="mt-2 m-1">
+        &#8451;
+      </span>
+    </div>
   </div>
 </template>
 
@@ -26,7 +55,13 @@ export default {
 
 <style scoped>
 .item-container {
-  width: 25%;  /* Set your desired width */
+  width: 25%;
+  /* Set your desired width */
   min-width: 200px;
+  justify-content: space-between;
+}
+
+.item-img {
+  height: 100%;
 }
 </style>
--- front/src/plugins/icons.js
+++ front/src/plugins/icons.js
@@ -1,9 +1,17 @@
 // https://materialdesignicons.com/
 /* eslint-disable*/
 import Loading from "vue-material-design-icons/Loading.vue"
+import LightBulb from "vue-material-design-icons/Lightbulb.vue"
+import LightbulbOutline from "vue-material-design-icons/LightbulbOutline.vue"
+import LightSwitch from "vue-material-design-icons/LightSwitch.vue"
+import LightSwitchOff from "vue-material-design-icons/LightSwitchOff.vue"
 
 export default {
   install: (app, options) => {
     app.component("loading", Loading)
+    app.component('lightbulb', LightBulb);
+    app.component('lightbulboff', LightbulbOutline);
+    app.component('lightswitch', LightSwitch);
+    app.component('lightswitchoff', LightSwitchOff);
   }
 }
