9a6b235915d1a04e6eec229d98809966a1de7356
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-28 12:23:05 +0200

    [front] room: add basic Room component

---
+++ front/src/components/cards/Room.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex m-3 py-2 px-5 justify-center rounded-md bg-stone-100 shadow-sm">
+    <h2
+      class="my-2 whitespace-nowrap"
+      :class="fontColor">
+      {{ room.name }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Room",
+  props: {
+    room: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
