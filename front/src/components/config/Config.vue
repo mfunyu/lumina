21599ae6f403137618da1854c54f713ba11ec943
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-28 11:57:55 +0200

    [front] config: add basig format

--- front/src/components/config/Config.vue
+++ front/src/components/config/Config.vue
@@ -1,5 +1,67 @@
 <template>
-  <div class="flex flex-col gap-5">
+  <div class="flex gap-5">
+    <div class="">
+      <p class="text-indigo-600 font-bold text-2xl">rooms</p>
 
+    </div>
+    <div class="">
+      <p class="text-indigo-600 font-bold text-2xl">entities</p>
+      <Item
+        v-for="entity in entities"
+        :key="entity.id"
+        :item="entity" />
+    </div>
   </div>
 </template>
+
+<script>
+import coreApi from "@/providers/core-api"
+import Item from "@/components/cards/Item"
+
+export default {
+  name: "Config",
+  components: { Item },
+  created() {
+    this.loadData()
+  },
+  data() {
+    return {
+      entities: [],
+      rooms: [],
+      isLoading: false,
+      isError: false,
+    }
+  },
+  methods: {
+    loadData() {
+      this.isLoading = true
+      this.isError = false
+
+      Promise.all([this.getEntities(), this.getRooms()])
+        .finally(() => {
+          this.isLoading = false
+        })
+    },
+    getEntities() {
+      return coreApi.glados.getEntities()
+        .then((entities) => {
+          this.entities = entities
+        })
+        .catch((error) => {
+          console.error(error)
+          this.isError = true
+        })
+    },
+    getRooms() {
+      return coreApi.glados.getRooms()
+        .then((rooms) => {
+          this.rooms = rooms
+        })
+        .catch((error) => {
+          console.error(error)
+          this.isError = true
+        })
+    }
+  }
+}
+</script>
